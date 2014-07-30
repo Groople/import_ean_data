f10381e923b1debbac28c4c9263c330d416ba1e0
Josh Schramm <josh.schramm@gmail.com>
2014-07-30 15:09:45 -0400

    Create initial setup_cron script

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'import_ean_data'
+maintainer       'Groople'
+maintainer_email 'josh.schramm@lvlsvn.com'
+license          'All rights reserved'
+description      'Creates a cron task for running the ean import'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/setup_cron.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: import_ean_data
+# Recipe:: setup_cron
+#
+# Copyright 2014, Groople, LLC
+#
+# All rights reserved - Do Not Redistribute
+#
+#
+node[:deploy].each do |app_name, deploy|
+  cron "import_ean_data" do
+    minute "0"
+    hour "1"
+    command "cd #{deploy[:deploy_to]}/current/docs/scripts && PARAM_MYSQL_USER=#{deploy[:database][:username]} PARAM_MYSQL_PASS=#{deploy[:database][:password]} PARAM_MYSQL_HOST=#{deploy[:database][:password]} PARAM_MYSQL_DB=#{deploy[:database][:database]} PARAM_HOME_DIR=/home/ubuntu ./EAN_MySQL_refresh.sh"
+  end
+end
